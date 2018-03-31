ad9c1a9bdadbe754b0859824c365a7e965198d77
Kevin <kevn.fu@gmail.com>
2018-03-30 18:15:10 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Gif-Tastic
+## Description
+Uses GIPHY to load gifs
+
+## Usage
+* Click buttons on the top to search for that term. 
+* Click More to load more gifs. 
+* Add new search term by using form. 
+* Clicking an image toggles animation. 
---
+++ script.js
@@ -0,0 +1,155 @@
+const key = "aExZtytookBSVzlAfYuyRa519qcywPU3";
+const limit = 10;
+
+class Storage {
+    get favs() {
+        let x = localStorage.getItem("favs"); 
+        x = Object.is(x, null) || Object.is(x, undefined) ? "[]" : x;
+        return JSON.parse(x);
+    }
+
+    set favs(data) {
+        localStorage.setItem("favs", JSON.stringify(data));
+    }
+
+    addFav(data) {
+        let x = this.favs;
+        x.push(data);
+        this.favs = x;
+    }
+
+    empty() {
+        this.favs = [];
+    }
+}
+
+let topics = ["dog", "cat", "hampster", "turtle", "goldfish"];
+let storage = new Storage();
+let storedResponse, currentQuery;
+
+function renderButtons() {
+    $("#buttons").empty();
+    topics.forEach(e => {
+        $("#buttons").append($("<button>")
+            .attr("type", "button")
+            .addClass("button")
+            .html(e));
+    });
+}
+
+class Image {
+    constructor(data) {
+        this.data = data;
+        this.play = false;
+        this.isFav = false;
+        this.img = $("<img>")
+            .attr("src", data.images.fixed_height_still.url)
+            // on image click, toggle play
+            .click(this.togglePlay.bind(this));
+        this.div = $("<div>")
+            .addClass("gif")
+            .append(this.img)
+            .append($("<br>"))
+            .append($("<p>").html("Rating: " + data.rating));
+    }
+
+    togglePlay() {
+        this.play = !this.play;
+        if(this.play) {
+            this.img.attr("src", this.data.images.fixed_height.url);
+        } else {
+            this.img.attr("src", this.data.images.fixed_height_still.url);
+        }
+        return this;
+    }
+
+    withFavButton() {
+        this.div.append($("<button>")
+            .html("Add to Fav")
+            .click(() => {
+                this.div.children().last().remove(); // remove button
+                this.isFav = true;
+                storage.addFav(this.data); // store in localstorage
+                this.display();
+            }));
+        return this;
+    }
+
+    setFav(b) {
+        this.isFav = b;
+        return this;
+    }
+
+    display() {
+        if(this.isFav) {
+            $("#favs").append(this.div);
+        } else {
+            $("#display").append(this.div);
+        }
+        return this;
+    }
+}
+
+
+// loads up to 10 images. hides l#oad button if no more to load.
+function loadImages(query, offset) {
+    $("#load").hide();
+
+    $.ajax({
+        url: "http://api.giphy.com/v1/gifs/search?" + $.param({
+            api_key: key, 
+            q : query, 
+            limit: limit,
+            offset: offset
+        }),
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        response.data.forEach(function(e) {
+            new Image(e).withFavButton().display();
+        })
+
+        // save response object
+        storedResponse = response;
+
+        // manage #load button visibility
+        let p = storedResponse.pagination;
+        if(p.total_count >= p.offset + limit) {
+            $("#load").show();
+        }
+    });
+}
+
+$(document).on("click", ".button", function() {
+    $("#display").empty();
+    $("#load").hide();
+    numLoaded = 0;
+    currentQuery = $(this).html();
+    loadImages(currentQuery, 0);
+});
+
+$(document).ready(function() {
+    renderButtons();
+    
+    // load saved favorites
+    // console.log("initial storage: " + JSON.stringify(storage.favs));
+    storage.favs.forEach((e) => {
+        new Image(e).setFav(true).display();
+    });
+
+    $("#submit").click(function(event) {
+        event.preventDefault();
+
+        topics.push($("#input").val());
+        renderButtons();
+    });
+
+    $("#load").click(function() {
+        loadImages(currentQuery, storedResponse.pagination.offset + limit);
+    });
+
+    $("#clear-fav").click(function() {
+        storage.empty();
+        $("#favs").empty();
+    });
+});
